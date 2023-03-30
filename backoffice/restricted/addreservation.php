4bd88a4f5161ca7ef9e3d4d97798e3b362b70cbd
LeonElMatador <leon.lepers@gmail.com>
2023-03-30 15:19:58 +0200

    oups

---
+++ backoffice/restricted/addreservation.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/media/logo.png">
+    <!-- <link rel="stylesheet" href="assets/style/style.css"> -->
+    <link href="https://fonts.googleapis.com/css?family=Proxima+Nova:400,700&display=swap" rel="stylesheet">
+
+    <title><?php echo isset($_POST["titre"]) ? urldecode($_POST["titre"]) : "ADMIN" ?></title>
+</head>
+
+<body>
+    <a href="home.php">Accueil</a>
+    <a href="projection.php">Projections</a>
+    <a href="film.php">Debug Film</a>
+    <a href="reservation.php">Debug Reservation</a>
+
+    <div name='info-film'>
+        </br>
+        <?php 
+        if (isset($_POST["nofilm"])){
+            $erreur = false;
+            $bdd = new PDO("mysql:host=localhost;dbname=bdcinevieillard-lepers;charset=utf8", "root", "");
+
+            //Génération de la requête qui va chercher dans la DB les projections correspondant au film renseigné
+            $requete = ("select * from film natural join public where nofilm=$_POST[nofilm]");
+            // Préparation de la requête en utilisant la variable préparée auparavant
+            $req = $bdd->prepare($requete);
+            $req->execute();
+            // Recherche pour chaque film de la base de données si ses caractéristiques correspondent à celles recherchées
+            $uneligne = $req->fetch();
+
+            if ($uneligne){
+                echo "<table cellpadding='5'>";
+                    echo "<tr>";
+                    echo "<td rowspan='7'><img src='../../assets/media/affiches/$uneligne[imgaffiche]' width=200px></td>";
+                    echo "<td>$uneligne[titre]</td>";
+                    echo "</tr>";
+                    echo "<tr><td>Durée : ".str_replace(":","h",date('G:i', strtotime($uneligne["duree"])))."</td></tr>";
+                    echo "<tr><td>$uneligne[realisateurs]</td></tr>";
+                    echo "<tr><td>$uneligne[acteurs]</td></tr>";
+                    echo "<tr><td>$uneligne[infofilm]</td></tr>";
+                    echo "<tr><td>$uneligne[libpublic]</td></tr>";
+                    echo "<tr><td colspan='2'>$uneligne[synopsis]</td></tr>";
+                echo "</table>";
+            }else{
+                echo "Erreur : film inconnu";
+                $erreur = true;
+            }
+
+            $req->closeCursor();
+        }else{
+            echo "Erreur : problème de film sélectionné";
+            $erreur = true;
+        }
+        ?>
+    </div>
+    <div name='seance-film'>
+        </br>
+        <?php 
+        if (!$erreur){
+            echo "<h1>Séances</h1>";
+            
+
+            if(isset($_POST["nofilm"])) {
+                //Génération de la requête qui va chercher dans la DB les projections correspondant au film renseigné
+                $requete = ("select * from projection where nofilm=$_POST[nofilm] ORDER BY dateproj, heureproj");
+            }
+            // Préparation de la requête en utilisant la variable préparée auparavant
+            $req = $bdd->prepare($requete);
+            $req->execute();
+            // Recherche pour chaque film de la base de données si ses caractéristiques correspondent à celles recherchées
+            $uneligne = $req->fetch();
+
+            if ($uneligne!=null){
+                $dateDeProj = $uneligne["dateproj"];
+                $date = date('l j F Y', strtotime($uneligne["dateproj"]));
+                echo "Projections du $date :";
+                echo "<table cellpadding='5'>";
+                echo "<tr>";
+                    echo "<th>Horaire</th>";
+                    echo "<th>Informations séance</th>";
+                    echo "<th>Places disponibles</th>";
+                echo "</tr>";
+            }
+        
+            while ($uneligne!=null)
+            {
+                $requete2 = ("select (select nbplaces from salle natural join projection where noproj=$uneligne[noproj]) - COALESCE((select sum(nbplacesresa) from reservation where noproj=$uneligne[noproj]),0) as nbplacerestante, nbplaces from salle natural join projection where noproj=$uneligne[noproj]");
+                $req2 = $bdd->prepare($requete2);
+                $req2->execute();
+                $uneligne2 = $req2->fetch();
+                
+                if ($dateDeProj!=$uneligne["dateproj"]){
+                    echo "</table>";
+                    $dateDeProj = $uneligne["dateproj"];
+                    $date = date('l j F Y', strtotime($uneligne["dateproj"]));
+                    echo "<br/>Projections du $date :";
+                    echo "<table cellpadding='5'>";
+                    echo "<tr>";
+                        echo "<th>Horaire</th>";
+                        echo "<th>Informations séance</th>";
+                        echo "<th>Places disponibles</th>";
+                    echo "</tr>";
+                }
+                
+                echo "<tr>";
+                echo "<td>";
+                echo str_replace(":","h",date('G:i', strtotime($uneligne["heureproj"])));
+                echo "</td>";
+                echo "<td>";
+                echo $uneligne["infoproj"];
+                echo "</td>";
+                echo "<td>";
+                if ($uneligne2["nbplacerestante"]>0){
+                    echo "$uneligne2[nbplacerestante] sur $uneligne2[nbplaces]";
+                    echo "<td><form method='post' action='addreservation.php'>";
+                    echo "<input type='hidden' name='noproj' value='$uneligne[noproj]'>";
+                    echo "<input type='hidden' name='nofilm' value='$_POST[nofilm]'>";
+
+                    echo "<button type='submit' name='btnsupprimer'>Supprimer cette séance</button>";
+                    echo "</form></td>";                
+                }else{
+                    echo ("Aucune place disponible");
+                }
+                echo "</td>";
+                echo "</tr>";
+                
+                $uneligne = $req->fetch();
+            }
+            echo "</table>";
+            $req->closeCursor();
+            // test
+                
+        }
+        ?>
+        <form method='post'>
+            <h1>Ajouter des séances</h1>
+
+            Informations sur la projection : <input type='text' name='txtInfo'
+                value='<?php isset($_POST["txtInfo"]) ? $_POST["txtInfo"] : '' ?>' />
+            </br>
+
+            </br>
+            <label for="salle-select">Salle :</label>
+            <select name="cbosalle">
+                <option value="" selected>Sélectionner une salle</option>
+                <?php
+                $bdd = new PDO("mysql:host=localhost;dbname=bdcinevieillard-lepers;charset=utf8", "root", "");
+
+                $req = $bdd->prepare("select * from salle");
+                $req->execute();
+                $uneligne = $req->fetch();
+                while ($uneligne!=null)
+                {
+                    if (isset($_POST["cbosalle"])==true && $_POST["cbosalle"]==$uneligne["nosalle"]){
+                        echo ("<option value=$uneligne[nosalle] selected>$uneligne[nosalle]</option>");
+                    }
+                    else 
+                    {
+                        echo ("<option value=$uneligne[nosalle]>$uneligne[nosalle]</option>");
+                    }
+                    $uneligne = $req->fetch();
+
+                }
+                
+                $req->closeCursor();
+             ?>
+            </select>
+            </br>
+
+            <?php 
+         if (isset($_POST["btnvalider"]) == true) {
+             echo "Date : <input type='date' name='date' value='$_POST[date]'/><br />";
+             echo "Heure : <input type='time' name='time' value='$_POST[time]'/><br />";
+
+         } else {
+             echo "Date : <input type='date' name='date' value='" . date("Y-m-d") . "' /><br />";
+             echo "Heure : <input type='time' name='time' value='" . date("H:i") . "' /><br />";
+
+         }
+         echo "<input type='hidden' name='nofilm' value='$_POST[nofilm]'>";
+
+         echo "<input type='submit' name='btnvalider' value='Rechercher'>";
+
+         if (isset($_POST["btnvalider"]) == true) {
+            $requete3="insert into projection values (null,'$_POST[date]','$_POST[time]','$_POST[txtInfo]',$_POST[nofilm],'$_POST[cbosalle]')";
+            echo $requete3;
+            $req3=$bdd->prepare($requete3);
+            $req3->execute();
+            echo "zouzou";
+         }
+
+         if(isset($_POST["btnsupprimer"])==true) {
+            $req4="delete from projection where noproj=$_POST[noproj]";
+            echo $req4;
+            $requete4=$bdd->prepare($req4);
+            $requete4->execute();
+        }
+
+        ?>
+    </div>
+
+    </form>
+</body>
+
+</html>
---
+++ backoffice/restricted/protected.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/media/logo.png">
+    <link rel="stylesheet" href="../../assets/style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Proxima+Nova:400,700&display=swap" rel="stylesheet">
+
+    <title>Accueil</title>
+</head>
+
+<body>
+    <a href="home.php">Accueil</a>
+    <a href="projection.php">Projections</a>
+    <a href="film.php">Debug Film</a>
+    <a href="reservation.php">Debug Reservation</a>
+
+    <div class="search-bar">
+        <form method="POST" action="protected.php">
+            <label for="title-input">Titre :</label>
+            <input type="text" name="txttitre"
+                value="<?php echo isset($_POST["txttitre"]) ? $_POST['txttitre'] : "" ?>" />
+
+            <label for="actor-input">Acteur :</label>
+            <input type="text" name="txtact" value="<?php isset($_POST["txtact"]) ? $_POST['txtact'] : "" ?>" />
+
+            <label for="director-input">Réalisateur :</label>
+            <input type="text" name="txtreal" value="<?php isset($_POST["txtreal"]) ? $_POST['txtreal'] : "" ?>" />
+
+            <label for="public-select">Public :</label>
+            <select name="cbopublic">
+                <option value="" selected>Sélectionner un public</option>
+                <?php
+                $bdd = new PDO("mysql:host=localhost;dbname=bdcinevieillard-lepers;charset=utf8", "root", "");
+
+                $req = $bdd->prepare("select * from public");
+                $req->execute();
+                $uneligne = $req->fetch();
+                while ($uneligne!=null)
+                {
+                    if (isset($_POST["cbopublic"])==true && $_POST["cbopublic"]==$uneligne["nopublic"]){
+                        echo ("<option value=$uneligne[nopublic] selected>$uneligne[libpublic]</option>");
+                    }
+                    else 
+                    {
+                        echo ("<option value=$uneligne[nopublic]>$uneligne[libpublic]</option>");
+                    }
+                    $uneligne = $req->fetch();
+
+                }
+                
+                $req->closeCursor();
+                $bdd=null;
+             ?>
+            </select>
+            <label for="genre-select">Genre :</label>
+            <select name="cbogenres[]" multiple>
+                <?php
+                $bdd = new PDO("mysql:host=localhost;dbname=bdcinevieillard-lepers;charset=utf8", "root", "");
+
+                $req = $bdd->prepare("select * from genre");
+                $req->execute();
+                $uneligne = $req->fetch();
+                while ($uneligne!=null)
+                {
+                    echo ("<option value='$uneligne[nogenre]'>$uneligne[libgenre]</option>");
+                    $uneligne = $req->fetch();
+
+                }
+                
+                $req->closeCursor();
+                $bdd=null;
+             ?>
+            </select>
+
+
+            <input type="submit" name="btnvalider" value="Rechercher">
+        </form>
+    </div>
+    <?php 
+    $bdd = new PDO("mysql:host=localhost;dbname=bdcinevieillard-lepers;charset=utf8", "root", "");           
+    //Génération de la première requête dans une variable en string avec uniquement le titre, les réalisateurs et acteurs
+    $titre = isset($_POST["txttitre"]) ? addslashes($_POST['txttitre']) : "";
+    $acteurs = isset($_POST["txtact"]) ? addslashes($_POST['txtact']) : "";
+    $realisateurs = isset($_POST["txtreal"]) ? addslashes($_POST['txtreal']) : "";
+
+    $requete = ("select distinct film.* from film natural join concerner where titre like\"%$titre%\" and acteurs like\"%$acteurs%\"
+                                and realisateurs like\"%$realisateurs%\" ");
+    //Si un public est renseigné, ajoute la recherche du public à la requête
+    if(isset($_POST["cbopublic"]) == true && $_POST["cbopublic"] != "") {
+        $requete.= (" and nopublic='$_POST[cbopublic]' ");
+    }
+    // Pour chaques genres sélectionnés, ceux-ci sont rajoutés à la requête
+    if(isset($_POST["cbogenres"]) == true && $_POST["cbogenres"] != "") {
+        $requete.= (" and nogenre IN (");
+        for ($i=0;$i<count($_POST["cbogenres"]);$i++)  
+        {
+            $requete.= ($_POST["cbogenres"][$i].", ");
+        }
+        $requete = substr($requete, 0, -2).")";
+    }
+    // Préparation de la requête en utilisant la variable préparée auparavant
+    $req = $bdd->prepare($requete);
+    $req->execute();
+    // Recherche pour chaque film de la base de données si ses caractéristiques correspondent à celles recherchées
+    $uneligne = $req->fetch();
+
+    if (!$uneligne){
+        echo "Aucun film ne correspond à votre recherche";
+    }
+    while ($uneligne)
+    {
+        echo "<label>";
+        echo "<table cellpadding='5' onclick=\"document.getElementById('form_$uneligne[nofilm]').submit()\">";
+        echo "<tr>";
+        echo "<td rowspan='7'><img src='../../assets/media/affiches/$uneligne[imgaffiche]' width=100px></td>";
+        echo "<td>$uneligne[titre]</td>";
+        echo "</tr>";
+        echo "<tr><td>Durée : ".str_replace(":","h",date('G:i', strtotime($uneligne["duree"])))."</td></tr>";
+        echo "<tr><td>$uneligne[realisateurs]</td></tr>";
+        echo "<tr><td>$uneligne[acteurs]</td></tr>";
+        echo "</table>";
+        echo "<form id='form_$uneligne[nofilm]' method='post' action='addreservation.php'>";
+        echo "<input type='hidden' name='nofilm' value='$uneligne[nofilm]'>";
+        echo "<input type='hidden' name='titre' value='".urlencode($uneligne["titre"])."'>";
+        echo "</form>";
+        echo "</label>";
+        $uneligne = $req->fetch();
+    }
+    $req->closeCursor();
+
+    $bdd=null;
+
+?>
+</body>
+
+</html>
